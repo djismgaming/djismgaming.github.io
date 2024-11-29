812f623695ee0a38d05158f3e485486454637e07
djismgaming <ismaellambertymuniz@gmail.com>
2024-11-29 02:14:07 -0400

    New post added on 2024-11-29 02:14:07

---
+++ blogupdate.ps1
@@ -0,0 +1,24 @@
+# Define source and destination paths
+$source = "C:\Users\Ismael\Documents\notes\sites\djismgaming.github.io\posts\"
+$destination = "C:\Users\Ismael\Documents\GitHub\djismgaming.github.io\content\posts"
+
+# Run robocopy with /MIR option
+robocopy $source $destination /MIR
+
+# Navigate to the Git repository's root
+$repoRoot = "C:\Users\Ismael\Documents\GitHub\djismgaming.github.io"
+Set-Location $repoRoot
+
+# Execute Hugo to build the site
+hugo
+
+# Stage all changes in Git
+git add .
+
+# Create a commit with a message including a timestamp
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$commitMessage = "New post added on $timestamp"
+git commit -m $commitMessage
+
+# Optional: Push changes to the remote repository
+git push
